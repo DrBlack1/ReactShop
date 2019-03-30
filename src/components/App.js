9f91d68b4b5cceb4acf2274e48c778aba3e8b200
yourUserName <patrick.mack1@yahoo.com>
2019-03-30 17:48:31 +0000

    initiliase react template

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const shopitems = () => (
+  "Hello World"
+);
+
+class App extends Component {
+
+  render() {
+    return (<div>{shopitems}</div>)
+}
+}
+
+export default App;
